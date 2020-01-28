367bfcb105304bef28494dc44419a9e0371883c8
孙振宇 <sunzhenyu@live.com>
2020-01-28 10:06:34 +0800

    update validate

--- utils/validate.js
+++ utils/validate.js
@@ -1,13 +1,31 @@
 // created by webszy 2020-01-27
 
+
+
+const keyNum = obj => Object.keys(obj)
+const hasThisProp =val=>!['',null,undefined,'null','undefined'].includes(val)
 /**
  *
  * vaildator function
  * @function
- * @param {*} data
- * @param {*} rules
+ * @param {*} data -> req.body
+ * @param {*} rules ->[{target:'ONE',required:true,type:Number}]
  * @return Boolean
  */
-export function commonVaildator(data,rules){
-
+export function commonVaildator(data, rules) {
+  if (keyNum(data) === 0 || keyNum(rules) === 0) {
+    return {
+      result:false,
+      msg:'data or rules cannot be empty object'
+    }
+  }
+  for (const k of rules) {
+    const item = data[k.target]
+    if(k.required === true && !hasThisProp(item)){
+      return {
+        result:false,
+        msg:`${k.target}`
+      }
+    }
+  }
 }
