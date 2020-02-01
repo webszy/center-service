2e5b483ce948fc51e2bbf915c093b124483b5f3e
孙振宇 <sunzhenyu@live.com>
2020-02-01 21:13:10 +0800

    use controller instead of express router splite

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const fs = require('fs-extra')
-const rfs = require('rotating-file-stream')
 const cors = require('cors')
 const hemlet = require('helmet')
 const morgan = require('morgan')
@@ -14,22 +13,11 @@ const port = process.env.PORT || 3000
 // const env = process.env.NODE_ENV
 
 // set logs
-const logFileName = (time, index) => {
-  if (!time) return 'access.log'
-  const pad = num => (num > 9 ? '' : '0') + num
-  const month = time.getFullYear() + '' + pad(time.getMonth() + 1)
-  const day = pad(time.getDate())
-  const hour = pad(time.getHours())
-  const minute = pad(time.getMinutes())
-  return `access-${month}/${month}${day}-${hour}${minute}-${index}.log`
-}
+
 const logDirectory = path.join(__dirname, config.logDirectory)
 // ensure log directory exists
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-const accessLogStream = rfs.createStream(logFileName, {
-  interval: '1d', // rotate daily
-  path: logDirectory
-})
+const accessLogStream = fs.createWriteStream(path.join(logDirectory, 'access.log'), { flags: 'a' })
 
 // express midware
 app.use(express.json())
@@ -44,8 +32,8 @@ app.set('view engine', 'art')
 // set static
 app.use(express.static(path.join(__dirname, 'public')))
 // import controllers (router handler function)
-const IndexController = require('./routes/index')
-const RegisterController = require('./routes/serviceRegister')
+const IndexController = require('./controller/home')
+const RegisterController = require('./controller/serviceRegister')
 // set router
 app.get('/',IndexController.get)
 app.post('/service',RegisterController.post)
